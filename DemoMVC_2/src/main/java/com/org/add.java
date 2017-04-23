4a592e8fae4f81364ddedaeba0d96f58feca7558
adirocks09 <adi9892@gmail.com>
2017-04-24 01:09:10 +0530

    Spring MVC

---
+++ DemoMVC_2/src/main/java/com/org/add.java
@@ -0,0 +1,16 @@
+package com.org;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class add {
+	
+	@RequestMapping("/login")
+	public void display(){
+		System.out.println("Hellooooooooooooo");
+	}
+		
+	}
+
+	
