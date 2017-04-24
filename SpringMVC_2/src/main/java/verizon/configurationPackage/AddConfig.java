79f2c48f0595bbfc963c27b048798d82d41ae9b9
adirocks09 <adi9892@gmail.com>
2017-04-25 04:59:17 +0530

    Spring Web MVC Java Configuration

---
+++ SpringMVC_2/src/main/java/verizon/configurationPackage/AddConfig.java
@@ -0,0 +1,22 @@
+package verizon.configurationPackage;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@ComponentScan({"verizon"})
+public class AddConfig {
+	
+//	@Bean
+//	public InternalResourceViewResolver viewResolver(){
+//		
+//		InternalResourceViewResolver vr=new InternalResourceViewResolver();
+//		vr.setPrefix("/WEB-INF/");
+//		vr.setSuffix(".jsp");
+//		
+//		return vr;
+//	}
+
+}
---
+++ SpringMVC_2/src/main/java/verizon/controllerPackage/AddController.java
@@ -0,0 +1,39 @@
+package verizon.controllerPackage;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import verizon.servicePackage.AddService;
+
+@Controller
+public class AddController {
+	
+	@RequestMapping("/add")
+	public ModelAndView add(HttpServletRequest request,HttpServletResponse response){
+		 
+		int n1=Integer.parseInt(request.getParameter("num1"));
+		int n2=Integer.parseInt(request.getParameter("num2"));
+		//int n3=n1+n2;  // directly adding the numbers
+		
+		AddService service1=new AddService();//calling service to add the numbers
+		int n3=service1.add(n1, n2);
+		
+
+		ModelAndView view=new ModelAndView();
+		
+		view.addObject("sum",n3);
+		view.setViewName("display.jsp");
+		//view.setViewName("display2");
+		
+		
+
+		System.out.println("Result="+n3);
+		return view;
+	}
+
+
+}
