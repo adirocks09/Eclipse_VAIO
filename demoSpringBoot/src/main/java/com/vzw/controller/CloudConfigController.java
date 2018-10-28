1dce8199255572a7646a76384961fc33e53e938e
adirocks09 <adi9892@gmail.com>
2018-10-28 17:22:58 +0530

    Cloud-Config-Client

--- demoSpringBoot/src/main/java/com/vzw/controller/CloudConfigController.java
+++ demoSpringBoot/src/main/java/com/vzw/controller/CloudConfigController.java
@@ -3,20 +3,23 @@ package com.vzw.controller;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/CloudConfig")
+@Controller
+@RequestMapping("/cloudConfig")
+@RefreshScope
 public class CloudConfigController {
 
-	@Value("${CloudConfig.message: Default Server }")	   // inject via application.properties
-	private String cloudConfigmessage;
+	@Value("${company.name: Default Server }")	   // inject via application.properties
+	private String companyName;
 	
-	@RequestMapping(value="/Home", method = RequestMethod.GET)
+	@RequestMapping(value="/company", method = RequestMethod.GET)
 	public String cloudConfig(Map<String, Object> model) {
-		model.put("cloudConfigmessage", this.cloudConfigmessage);
-			System.out.println(cloudConfigmessage);
-		return "cloudConfigmessage";
+		model.put("cloudConfigmessage", this.companyName);
+			System.out.println(companyName);
+		return "cloudConfigMessage";
 	}
 }
